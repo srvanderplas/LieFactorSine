4e8e08c3a73c6b67639e89f9c5a57d3a04644412
srvanderplas <srvanderplas@gmail.com>
2013-03-13 14:42:02 -0500

    moved theme_stimuli() to a different file.

--- code/GenerateZigZags.R
+++ code/GenerateZigZags.R
@@ -2,6 +2,7 @@
 require(ggplot2)
 require(plyr)
 require(reshape2)
+source("./code/themeStimuli.R")
 
 # Input number of line segments and a vector of nonnegative slope magnitudes. 
 # Output a dataset of x, y, slope, and segment number
@@ -20,20 +21,6 @@ getDataSet <- function(nsegment=4, slope=c(1))
   data$ynorm <- (data$y-mean(data$y))/sd(data$y)
   data
 }
-
-
-theme_stimuli <- function(base_size = 12, base_family = ""){
-                   theme_grey(base_size = base_size, base_family = base_family) +
-                   theme(legend.background = element_blank(), 
-                         legend.key = element_blank(), 
-                         panel.background = element_rect(fill="white",color="black"), 
-                         panel.border = element_blank(), 
-                         axis.title = element_blank(),
-                         strip.background = element_rect(fill="grey",color="black"), 
-                         plot.background = element_blank(),
-                         panel.grid.major = element_line(color="grey75"),
-                         axis.text=element_blank())
-                  }
 
 get4 <- function(order){
   ans <- which(order==1)
