9c1748f5687ba42bda57d160dbc39c47ba60e794
srvanderplas <srvanderplas@gmail.com>
2013-03-13 12:41:09 -0500

    Modifications to allow for automatic generation.

--- code/GenerateStimuli.R
+++ code/GenerateStimuli.R
@@ -10,13 +10,13 @@ getDataSet <- function(nsegment=4, slope=c(1))
   segmentLength=2:(nsegment+1)
   initialSlope <- rbind(rep(c(-1, 1), times=ceiling(nsegment/2))[1:nsegment], 
                         rep(c(1, -1), times=ceiling(nsegment/2))[1:nsegment])
-  x <- seq(1, sum(segmentLength), 1)
-  slope <- rep(slope, times=ceiling(nsegment/length(slope)))
+  x <- seq(1, sum(segmentLength), length=10*sum(segmentLength))
+  slope <- rep(slope, times=10*ceiling(nsegment/length(slope)))
   genY <- data.frame(slope = sample(slope, nsegment, replace=FALSE)*initialSlope[sample(c(1, 2), 1), ], 
                      times = sample(segmentLength, nsegment, replace=FALSE))
-  slope <- unlist(sapply(1:nsegment, function(i) rep(genY$slope[i], times=genY$times[i])))
-  segment <- unlist(sapply(1:nsegment, function(i) rep(i, times=genY$times[i])))
-  data <- data.frame(x=x, y=cumsum(slope), slope=slope, segmentLength=segment)
+  slope <- unlist(sapply(1:nsegment, function(i) rep(genY$slope[i], times=10*genY$times[i])))
+  segment <- unlist(sapply(1:nsegment, function(i) rep(i, times=10*genY$times[i])))
+  data <- data.frame(x=x, y=cumsum(slope)/10, slope=slope, segmentLength=segment)
   data$ynorm <- (data$y-mean(data$y))/sd(data$y)
   data
 }
@@ -28,14 +28,33 @@ theme_stimuli <- function(base_size = 12, base_family = ""){
                          legend.key = element_blank(), 
                          panel.background = element_rect(fill="white",color="black"), 
                          panel.border = element_blank(), 
+                         axis.title = element_blank(),
                          strip.background = element_rect(fill="grey",color="black"), 
                          plot.background = element_blank(),
                          panel.grid.major = element_line(color="grey75"),
                          axis.text=element_blank())
                   }
 
-slopes <- list(c(1), c(1), c(1), c(1.5, 1))
-data <- do.call("rbind", lapply(sample(1:4, 4), function(i) cbind(getDataSet(8, slope=unlist(slopes[i])), set=i)))
+get4 <- function(order){
+  ans <- which(order==1)
+  a <- .75*(order==1) + .95*(order!=1)
+  b <- 2*(order==1) + 1.05*(order!=1)
+  nslopes <- 4
+  nseg <- 8
+  do.call("rbind", lapply(1:length(order), function(i) data.frame(
+    getDataSet(nseg, slope=runif(nslopes, min=a[i], max=b[i])),
+    set=i, correct=ans)))
+}
 
+data <- get4(sample(1:4, 4))
 
+data.points <- ddply(data, .(set, x, segmentLength, correct, slope), summarise, ynorm=runif(2, ynorm-.5, ynorm+.5))
+
+# Ribbon plot
 qplot(data=data, x=x, ymin=ynorm-.5, ymax=ynorm+.5, geom="ribbon") + facet_wrap(~set) + theme_stimuli()
+
+# Points
+qplot(data=data.points, x=x, y=ynorm, geom="point", alpha=I(.25), colour=I("grey10")) + 
+  geom_line(data=data, aes(x=x, y=ynorm), color="black") +
+  facet_wrap(~set) + theme_stimuli()
+
