478b0a27b1ced9ee074dce760fa54884da27969f
srvanderplas <srvanderplas@gmail.com>
2013-03-18 16:32:39 -0500

    Paper updates, additional code for demonstrating 3d perspective + sine illusion.

--- code/SineIllusionDemo.R
+++ code/SineIllusionDemo.R
@@ -5,7 +5,48 @@ trans3d <- function(x,y,z, plot) {
   list(x=tmat[1,]/tmat[4,], y=tmat[2,]/tmat[4,])
 } 
 
+createSine <- function(n=200, len=1, f=f, fprime=fprime, f2prime=f2prime, a=0, b=2*pi) {
+  x <- seq(a, b, length=n+2)[(2:(n+1))]
+  ell <- rep(len, length=length(x))
+  fx <- f(x)
+  ystart <- fx - .5*ell
+  yend <- fx + .5*ell
+  
+  # now correct for line illusion in vertical direction
+  dy <- diff(range(fx))
+  dx <- diff(range(x))
+  # fprime works in framework of dx and dy, but we represent it in framework of dx and dy+len
+  # needs to be fixed by factor a:  
+  a <- dy/(dy + len) 
+  # ellx is based on the "trig" correction
+  ellx <- ell / cos(atan(abs(a*fprime(x))))
+  # ellx2 is based on linear approximation of f  
+  ellx2 <- ell * sqrt(1 + a^2*fprime(x)^2)
+  
+  # make this a data frame - ggplot2 doesn't do well with floating vectors
+  dframe <- data.frame(x=x, xstart=x, xend=x, y=fx, ystart=ystart, yend=yend, ell=ell, ellx = ellx, ellx2=ellx2)
+  
+  # third adjustment is based on quadratic approximation of f.
+  # this needs two parts: correction above and below f(x)  
+  
+  fp <- a*fprime(x)
+  f2p <- a*f2prime(x)
+  lambdap <- (sqrt((fp^2+1)^2-f2p*fp^2*ell) + fp^2 + 1)^-1    
+  lambdam <- -(sqrt((fp^2+1)^2+f2p*fp^2*ell) + fp^2 + 1)^-1    
+  
+  
+  dframe$ellx4.l <- (4*abs(lambdap)*sqrt(1+fp^2))^-1
+  dframe$ellx4.u <- (4*abs(lambdam)*sqrt(1+fp^2))^-1
+  
+  dframe
+}
 
+f <- function(x) 2*sin(x)
+fprime <- function(x) 2*cos(x)
+f2prime <- function(x) -2*sin(x)
+
+# Attempt at making "self-luminous" images as in Gregory (1968) "Perceptual Illusions and Brain Models." Proc Roy. Soc. B 171 (279-296).
+# Not sure what's required for "self luminous" images, but this doesnt really fix it for me... 
 qplot(x=x, xend=xend, y = ystart, yend=yend, geom="segment", data=createSine(40, 1, f=f, fprime=fprime, f2prime), colour=I("white"), size=I(2)) +
   geom_line(aes(x=x, y=ystart), colour="white") + 
   geom_line(aes(x=x, y=yend), colour="white") + 
@@ -15,6 +56,7 @@ qplot(x=x, xend=xend, y = ystart, yend=yend, geom="segment", data=createSine(40,
         axis.text = element_blank()) + coord_equal(ratio=1)
 
 
+# 3-dimensionality seems to be a function of the curve mismatch or the gradient (or both) - it's not evident here.
 x <- seq(-6, 6, .1)
 y <- cumsum(sign(cos(x))*rep(.1, length(x)))
 qplot(x,y, geom="line") + geom_line(aes(x=x, y=y+1))
@@ -22,26 +64,51 @@ qplot(x,y, geom="line") + geom_line(aes(x=x, y=y+1))
 x <- seq(0, 2*pi, length=40)
 data <- do.call("rbind", lapply(seq(-.5, .5, .5), function(i) data.frame(x=x, y=2*sin(x), z=i)))
 
-qplot(data=data, x, y, geom="line", colour=z, group=z) + scale_colour_continuous(guide="none", high="grey30", low="grey60") + 
-  geom_segment(data=createSine(40, 1, f=f, fprime=fprime, f2prime, a=0, b=2*pi), aes(x=x, y=ystart, xend=xend, yend=yend), inherit.aes=FALSE)
+# Two-dimensional plot, where y_plot = y+z
+qplot(data=data, x, y=y+z, geom="line", colour=z, group=z) + scale_colour_continuous(guide="none", high="grey30", low="grey60") + 
+  geom_segment(data=createSine(40, 1, f=f, fprime=fprime, f2prime, a=0, b=2*pi), aes(x=x, y=ystart, xend=xend, yend=yend), inherit.aes=FALSE) + 
+  theme(panel.grid.major=element_blank(), panel.background = element_rect(fill = "white", colour = "black"),
+        panel.grid.minor=element_blank(), panel.background=element_blank(),
+        axis.title = element_blank(), axis.ticks = element_blank(), 
+        axis.text = element_blank()) + coord_equal(ratio=1)
 
 data.persp <- acast(data, x~z, value.var="y")
-x <- unique(data$x)
-y <- unique(data$z)
+x <- sort(unique(data$x))
+y <- sort(unique(data$y))
+z <- sort(unique(data$z))
 
 
-persp(x, y, data.persp, ylab="z", zlab="y", theta=-10, phi=35, border=NA, shade=.75) 
+persp(x, z, data.persp, ylab="z", zlab="y", theta=-10, phi=35, border=NA, shade=.75) 
 # Extant width makes perfect sense here - it's exactly what you'd be judging if you were judging width for a perspective view, 
 # except that you would have more information and/or be able to assume some sort of width constancy behavior. 
 # It's just when we look at purely 2d things that the extant width doesn't make sense anymore - because there is no 3d context. 
 
 
-persp(x, y, data.persp, ylab="z", zlab="y", theta=-10, phi=30, border=NA, shade=.5) 
+persp(x, z, data.persp, ylab="z", zlab="y", theta=-7, phi=30, border=NA, shade=.5) 
 # Note how ambiguous this figure could be - is the "wave" in z or y? is 0-pi the "near" region, or is it further away with pi-2*pi as the "near" region? It also seems to "flip" like a necker cube, at least to me.
 
-persp(x, y, data.persp, ylab="z", zlab="y", theta=-10, phi=30, shade=.5) 
+persp(x, z, data.persp, ylab="z", zlab="y", theta=-7, phi=30, shade=.5) 
 # Adding in the lines helps somewhat, but still doesn't completely resolve it.
 
+
+x <- seq(0, 2*pi, length=40)
+data2 <- do.call("rbind", lapply(x, function(i) data.frame(x=i, y=c(-.5, .5), z=2*sin(i) +c(-.5, .5))))
+
+data2.persp <- acast(data2, x~y, value.var="z")
+x2 <- sort(unique(data2$x))
+y2 <- sort(unique(data2$y))
+z2 <- sort(unique(data2$z))
+
+# This is what pops out at me when things "flip" (the right side, at least...)
+persp(x2, y2, data2.persp, theta=0, phi=0, border=NA, shade=.5) 
+
+
+# Flipping 
+persp(x, z, data.persp, ylab="z", zlab="y", theta=0, phi=90, border="grey20")  # A
+persp(x, z, -data.persp, ylab="z", zlab="y", theta=0, phi=90, border="grey20") # B
+persp(x, z, data.persp, ylab="z", zlab="y", theta=10, phi=0, border="grey20")  # C
+persp(x, z, -data.persp, ylab="z", zlab="y", theta=0, phi=0, border="grey20")  # D
+# I seem to flip between B and C... 
 
 qplot(data=createSine(40, 1, f=f, fprime=fprime, f2prime, a=0, b=2*pi), x=x, xend=x, y=ystart, yend=yend, geom="segment") + 
   geom_line(aes(y=ystart), linetype=2) + geom_line(aes(y=yend), linetype=2) + 
@@ -53,6 +120,9 @@ qplot(data=createSine(40, 1, f=f, fprime=fprime, f2prime, a=0, b=2*pi), x=x, xen
         axis.text = element_blank()) + coord_equal(ratio=1)
 
 
-persp(x, y, data.persp, ylab="z", zlab="y", theta=0, phi=90, border="grey20") 
-persp(x, y, -data.persp, ylab="z", zlab="y", theta=0, phi=90, border="grey20") 
+p <- persp(x, z, -data.persp, ylab="z", zlab="y", theta=0, phi=90, border="grey20") # B
+lines(trans3d(x, .25*sin(x)-.375, 1+0*x, p), col="red")
+lines(trans3d(x, .25*sin(x)+.125, 1+0*x, p), col="red")
+
+
 
