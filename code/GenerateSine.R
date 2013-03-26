e6211c3819298161730aba4d966437d30f8a8b62
srvanderplas <srvanderplas@gmail.com>
2013-03-25 22:05:59 -0500

    Woohoo! Stimuli sets for bayes model detailed in /code/BayesAnalysisOfExperimentPower.R

--- code/GenerateSine.R
+++ code/GenerateSine.R
@@ -53,27 +53,36 @@ fprime <- function(x) 2*cos(x)
 f2prime <- function(x) -2*sin(x)
 orig <- createSine(50, 1, f, fprime, f2prime, 0, 2*pi)
 
-wstart <- seq(0, .9, by=.1)
-w.all <- t(sapply(1:length(wstart), function(i) sort(c(0, 1, seq(wstart[i], wstart[i]+.3, by=.1)))))
+w.all <- rbind(seq(0, 1.3, len=6), 
+           c(0, seq(.2, .6, len=4), 1.3), 
+           c(0, seq(.5, .9, len=4), 1.3), 
+           c(0, seq(.8, 1.2, len=4), 1.3),
+           c(0, .4, .5, .6, .7, 1.3), c(0, .5, .6, .7, .8, 1.3), c(0, .6, .7, .8, .9, 1.3), 
+           c(0, .7, .8, .9, 1, 1.3), c(0, .8, .9, 1, 1.1, 1.3), c(.4, .7, .75, .8, .85, 1))
 
-
+data.file <- NULL
 for(j in 1:10){
-  w <- w.all[sample(1:nrow(w.all), 1),]
-  diff.w <- sum((.05/diff(w)))
+  w <- w.all[j,]
+  diff.w <- min(ceiling((diff(w)/.05)))
   frameorder <- sample(w, 6)
   
   data <- rbind.fill(ldply(w, function(i) weightYTrans(orig, i)))
   data$set <- sapply(data$w, function(i) which(w %in% i))
   data$display <- sapply(data$w, function(i) which(frameorder %in% i))
-  data$jit <- rnorm(nrow(data), 0, .05)
-  qplot(data=data, x=xstart, y=ystart+jit, xend=xend, yend=yend+jit, geom="segment") + facet_wrap(~display) + coord_equal(ratio=1) + theme_stimuli()
-  ggsave(filename=paste("YTrans_Pic_", j, ".png", sep=""), path="./stimuli/SineYWeight", width=8, height=6, units="in")
-  key <- ddply(data, .(w, display), summarise, ratio.length=max(ellx4.l+ellx4.u)/min(ellx4.l+ellx4.u))
-  key <- key[order(key$display),]
-  write.table(key, paste("./stimuli/SineYWeight/YTrans_Table_", j, ".txt", sep=""), sep="\t", row.names=FALSE)
-  write.csv(data, paste("./stimuli/SineYWeight/YTrans_Data_", j, ".csv", sep=""), row.names=FALSE)
+  ans <- which.min(abs(frameorder-.99))
+  qplot(data=data, x=xstart, y=ystart, xend=xend, yend=yend, geom="segment") + facet_wrap(~display) + coord_equal(ratio=1) + theme_stimuli()
+  ggsave(filename=paste("YTransPic", j, ".png", sep=""), path="./stimuli", width=8, height=6, units="in")
+  data.file <- rbind(data.file, data.frame(
+    pic_id=j, 
+    sample_size=50, 
+    test_param="weight of y-transformation", 
+    param_value=paste("w", median(w), diff.w, sep="-"), 
+    obv_plot_location=ans,
+    pic_name=paste("YTransPic", j, ".png", sep=""),
+    experiment="turk8",
+    difficulty=diff.w))
 }
-
+write.csv(data.file, "./stimuli/datafile.csv", row.names=FALSE)
 
 # examples
 w <- c(0, 1, 1.2)
@@ -82,19 +91,19 @@ data <- rbind.fill(ldply(w, function(i) weightYTrans(orig, i)))
 data$set <- sapply(data$w, function(i) which(w %in% i))
 data$display <- sapply(data$w, function(i) which(frameorder %in% i))
 
-qplot(data=data, x=xstart, y=ystart, xend=xend, yend=yend, geom="segment") + facet_wrap(~display) + coord_equal(ratio=1) + theme_stimuli()
+qplot(data=data, x=xstart, y=ystart, xend=xend, yend=yend, geom="segment") + facet_wrap(~display) + theme_stimuli()
 ggsave("./stimuli/Examples/Example2.png",width=6, height=3)
 
 x <- seq(0, 2*pi, .1)
 data <- rbind(data.frame(x=x, y=x, ystart=x-.5, yend=x+.5, display=1),
               data.frame(x=x, y=x, ystart=.5*abs(x-pi/2)+.2, yend=1.5*abs(x-pi)-.2, display=2), 
               data.frame(x=x, y=x, ystart=x-sin(x), yend=x+sin(x), display=3))
-qplot(data=data, x=x, y=ystart, xend=x, yend=yend, geom="segment") + facet_wrap(~display) + coord_equal(ratio=1) + theme_stimuli()
+qplot(data=data, x=x, y=ystart, xend=x, yend=yend, geom="segment") + facet_wrap(~display) + theme_stimuli()
 ggsave("./stimuli/Examples/Example1.png",width=6, height=3)
 
 x <- seq(0, 2*pi, .1)
 data <- rbind(data.frame(x=x, xend=x, xstart=x, y=x, ystart=2*abs(x-pi)-.25, yend=2*abs(x-pi)+.25, display=3),
               data.frame(x=x, xend=x, xstart=x, y=x, ystart=x-.25+cos(x)*.25, yend=x+.25+cos(x)*.25 + x*.075, display=2),
-              data.frame(x=x, xend=x, xstart=x, y=x, ystart=x-.25+log(x+1), yend=x+.25+log(2*x+1), display=1))
-qplot(data=data, x=x, y=ystart, xend=x, yend=yend, geom="segment") + facet_wrap(~display) + coord_equal(ratio=1) + theme_stimuli()
+              data.frame(x=x, xend=x, xstart=x, y=x, ystart=x-.25+.5*log(x+1), yend=x+.25+.5*log(2*x+1), display=1))
+qplot(data=data, x=x, y=ystart, xend=x, yend=yend, geom="segment") + facet_wrap(~display) + theme_stimuli()
 ggsave("./stimuli/Examples/Example3.png",width=6, height=3)
